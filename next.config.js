4afca08132e867315d64c991f636b2d2dc23acd6
Claudio Barsante <clbmribas@gmail.com>
2021-08-23 19:00:54 -0300

    refactor: types folder

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  reactStrictMode: true,
-}
-
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+  reactStrictMode: true
+};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     disable: !isProd
   }
-})
+});
